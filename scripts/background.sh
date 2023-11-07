087e937333c0485e25253bf105f823e849fa8d64
Haru <parajogapaladins@gmail.com>
2023-11-06 23:17:20 -0300

    fix weird a single quotation mark pattern in the background.sh file (it was because some experimentation that i made but it's no need to be that way anymore

--- scripts/background.sh
+++ scripts/background.sh
@@ -2,34 +2,34 @@
 
 
 link=`playerctl -s metadata mpris:artUrl`
-link_without_https=''${link:24}''
+link_without_https="${link:24}"
 
-holder_name=''holder.jpg''
-blur_name=''blur.png''
-format=''.png''
-path=''$PWD/.background/''
+holder_name="holder.jpg"
+blur_name="blur.png"
+format=".png"
+path="$PWD/.background/"
 
-background_without_format=''${path}${link_without_https}''
-background_with_format=''${background_without_format}${format}''
-blur_image=''${path}${blur_name}''
-holder_image=''${path}${holder_name}''
+background_without_format="${path}${link_without_https}"
+background_with_format="${background_without_format}${format}"
+blur_image="${path}${blur_name}"
+holder_image="${path}${holder_name}"
 
 
 # download the picture with wget + spotify api album link
-if ! [ -e ''$background_with_format'' ]; then 
-  wget --quiet -P ''$path'' ''$link''
+if ! [ -e "$background_with_format" ]; then 
+  wget --quiet -P "$path" "$link"
 fi
 
 
-if [ ''${#background_without_format}'' -gt ''${#path}'' ];then 
-  if [ -e ''$background_without_format'' ]; then
-      # add ''.png'' on the end of the downloaded picture
-      if ! [ -e ''$background_with_format'' ]; then
-        mv ''$background_without_format'' ''$background_with_format''
+if [ "${#background_without_format}" -gt "${#path}" ];then 
+  if [ -e "$background_without_format" ]; then
+      # add ".png" on the end of the downloaded picture
+      if ! [ -e "$background_with_format" ]; then
+        mv "$background_without_format" "$background_with_format"
       fi
 		# delete every picture that is not of the current playing music
-		if [ -e ''$background_with_format'' ]; then
-		  find ''$path'' -type f ! -wholename ''$background_with_format'' ! -wholename ''$holder_image'' ! -wholename ''$blur_image'' -exec rm {} \;
+		if [ -e "$background_with_format" ]; then
+		  find "$path" -type f ! -wholename "$background_with_format" ! -wholename "$holder_image" ! -wholename "$blur_image" -exec rm {} \;
 		fi
   fi
 fi
